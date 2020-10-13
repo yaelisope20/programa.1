0c96a70818c17aaa6620ee98e8750732c24b7502
yaelisope20 <71914838+yaelisope20@users.noreply.github.com>
2020-10-12 22:24:25 -0500

    Add files via upload

---
+++ contador regresivo 1.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main()
+{
+    int res};
+    int in;
+    int na;
+    printf("dame el valor de na:\n");
+    scanf("%d",&na);
+    i=12;
+    rest=3*n+2*i;
+
+    while(res>=3*na)
+    {
+        printf("%d",res);
+        res=res-1;
+    }
+    printf("\nfinal del ciclo\n");
+    return 0;
+}
